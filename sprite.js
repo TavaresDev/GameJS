435314971e2810f3129fe6e8f4cc171ede73127e
TavaresDev <vtavares.andre@gmail.com>
2019-02-19 00:42:05 -0300

    jogo pronto

--- sprite.js
+++ sprite.js
@@ -1,8 +1,8 @@
 function Sprite(x, y, largura, altura) {
-	this.x = x
-	this.y = y
-	this.largura = largura
-	this.altura = altura
+	this.x = x;
+	this.y = y;
+	this.largura = largura;
+	this.altura = altura;
 
 	this.desenha = function(xcanvas, ycanvas){
 		ctx.drawImage(img, this.x, this.y, this.largura, this.altura, xcanvas, ycanvas, this.largura, this.altura);
@@ -11,6 +11,7 @@ function Sprite(x, y, largura, altura) {
 
 var bg = new Sprite(0, 0, 600, 600),
 spriteBoneco = new Sprite(618, 16, 87, 87),
+spriteBoneco2 = new Sprite(721, 13, 92, 92),
 
 perdeu = new Sprite (603, 478, 397, 358),
 jogar = new Sprite (603, 127, 397, 347),
